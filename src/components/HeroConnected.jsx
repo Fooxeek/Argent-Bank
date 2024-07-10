d0ee6cd93fa26a208eca7b5ff81fd86b395599b1
fooxeek <foxeek02@gmail.com>
2024-07-10 16:41:35 +0200

    feat: Add user authentication redirect

--- src/components/HeroConnected.jsx
+++ src/components/HeroConnected.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import EditButton from "./EditButton";
 
 export default function HeroConnected() {
   const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/sign-in");
+    }
+  }, [user, navigate]);
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <div className="font-semibold flex justify-center items-center flex-col">
